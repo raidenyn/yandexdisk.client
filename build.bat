88a96817a5f3006b6c534994e8b47a00563baf48
Yuriy Nagaev <yuriy@nagaev.me>
2016-02-04 00:55:28 +0300

    Added build file for MyGet.

---
+++ build.bat
@@ -0,0 +1,21 @@
+﻿@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+set version=
+if not "%PackageVersion%" == "" (
+   set version=-Version %PackageVersion%
+)
+
+REM Package restore
+call %NuGet% restore src\YandexDisk.Client\packages.config -OutputDirectory %cd%\src\packages -NonInteractive
+call %NuGet% restore src\YandexDisk.Client.Tests\packages.config -OutputDirectory %cd%\src\packages -NonInteractive
+
+REM Build
+"%MsBuildExe%" src\YandexDisk.Client.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+
+REM Package
+mkdir Build
+call %nuget% pack "src\YandexDisk.Client.nuspec" -symbols -o Build -p Configuration=%config% %version%
