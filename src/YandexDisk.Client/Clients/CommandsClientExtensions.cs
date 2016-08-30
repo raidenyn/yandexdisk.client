f360ca8f218ef0e95c1a9a5964ce8cf253087614
Yuriy nagaev <yuriy@nagaev.me>
2016-08-31 01:45:49 +0300

    Changed parameters type of waiting methods for command operations.

--- src/YandexDisk.Client/Clients/CommandsClientExtensions.cs
+++ src/YandexDisk.Client/Clients/CommandsClientExtensions.cs
@@ -13,12 +13,22 @@ namespace YandexDisk.Client.Clients
     [PublicAPI]
     public static class CommandsClientExtensions
     {
-        private static async Task WaitOperationAsync([NotNull] this ICommandsClient client, [NotNull] Link operationLink, CancellationToken cancellationToken, int pullPeriod)
+        /// <summary>
+        /// Default pull period for waiting operation.
+        /// </summary>
+        public static TimeSpan DefaultPullPeriod = TimeSpan.FromSeconds(3);
+
+        private static async Task WaitOperationAsync([NotNull] this ICommandsClient client, [NotNull] Link operationLink, CancellationToken cancellationToken, TimeSpan? pullPeriod)
         {
+            if (pullPeriod == null)
+            {
+                pullPeriod = DefaultPullPeriod;
+            }
+
             Operation operation;
             do
             {
-                Thread.Sleep(TimeSpan.FromSeconds(pullPeriod));
+                Thread.Sleep(pullPeriod.Value);
                 operation = await client.GetOperationStatus(operationLink, cancellationToken).ConfigureAwait(false);
             } while (operation.Status == OperationStatus.InProgress &&
                      !cancellationToken.IsCancellationRequested);
@@ -27,8 +37,7 @@ namespace YandexDisk.Client.Clients
         /// <summary>
         /// Copy file or folder on Disk from one path to another and wait until operation is done
         /// </summary>
-        /// <returns></returns>
-        public static async Task CopyAndWaitAsync([NotNull] this ICommandsClient client, [NotNull] CopyFileRequest request, CancellationToken cancellationToken = default(CancellationToken), int pullPeriod = 3)
+        public static async Task CopyAndWaitAsync([NotNull] this ICommandsClient client, [NotNull] CopyFileRequest request, CancellationToken cancellationToken = default(CancellationToken), TimeSpan? pullPeriod = null)
         {
             var link = await client.CopyAsync(request, cancellationToken).ConfigureAwait(false);
 
@@ -41,8 +50,7 @@ namespace YandexDisk.Client.Clients
         /// <summary>
         /// Move file or folder on Disk from one path to another and wait until operation is done
         /// </summary>
-        /// <returns></returns>
-        public static async Task MoveAndWaitAsync([NotNull] this ICommandsClient client, [NotNull] MoveFileRequest request, CancellationToken cancellationToken = default(CancellationToken), int pullPeriod = 3)
+        public static async Task MoveAndWaitAsync([NotNull] this ICommandsClient client, [NotNull] MoveFileRequest request, CancellationToken cancellationToken = default(CancellationToken), TimeSpan? pullPeriod = null)
         {
             var link = await client.MoveAsync(request, cancellationToken).ConfigureAwait(false);
 
@@ -55,8 +63,7 @@ namespace YandexDisk.Client.Clients
         /// <summary>
         /// Delete file or folder on Disk and wait until operation is done
         /// </summary>
-        /// <returns></returns>
-        public static async Task DeleteAndWaitAsync([NotNull] this ICommandsClient client, [NotNull] DeleteFileRequest request, CancellationToken cancellationToken = default(CancellationToken), int pullPeriod = 3)
+        public static async Task DeleteAndWaitAsync([NotNull] this ICommandsClient client, [NotNull] DeleteFileRequest request, CancellationToken cancellationToken = default(CancellationToken), TimeSpan? pullPeriod = null)
         {
             var link = await client.DeleteAsync(request, cancellationToken).ConfigureAwait(false);
 
@@ -69,8 +76,7 @@ namespace YandexDisk.Client.Clients
         /// <summary>
         /// Empty trash
         /// </summary>
-        /// <returns></returns>
-        public static async Task EmptyTrashAndWaitAsyncAsync([NotNull] this ICommandsClient client, [NotNull] string path, CancellationToken cancellationToken = default(CancellationToken), int pullPeriod = 3)
+        public static async Task EmptyTrashAndWaitAsyncAsync([NotNull] this ICommandsClient client, [NotNull] string path, CancellationToken cancellationToken = default(CancellationToken), TimeSpan? pullPeriod = null)
         {
             var link = await client.EmptyTrashAsync(path, cancellationToken).ConfigureAwait(false);
 
@@ -83,8 +89,7 @@ namespace YandexDisk.Client.Clients
         /// <summary>
         /// Restore files from trash
         /// </summary>
-        /// <returns></returns>
-        public static async Task RestoreFromTrashAndWaitAsyncAsync([NotNull] this ICommandsClient client, [NotNull] RestoreFromTrashRequest request, CancellationToken cancellationToken = default(CancellationToken), int pullPeriod = 3)
+        public static async Task RestoreFromTrashAndWaitAsyncAsync([NotNull] this ICommandsClient client, [NotNull] RestoreFromTrashRequest request, CancellationToken cancellationToken = default(CancellationToken), TimeSpan? pullPeriod = null)
         {
             var link = await client.RestoreFromTrashAsync(request, cancellationToken).ConfigureAwait(false);
 
@@ -93,5 +98,55 @@ namespace YandexDisk.Client.Clients
                 await client.WaitOperationAsync(link, cancellationToken, pullPeriod).ConfigureAwait(false);
             }
         }
+
+
+        #region Obsoleted
+        /// <summary>
+        /// Copy file or folder on Disk from one path to another and wait until operation is done
+        /// </summary>
+        [Obsolete("Method is obsolete. Please use CopyAndWaitAsync(ICommandsClient, CopyFileRequest, CancellationToken, TimeSpan) instead.")]
+        public static Task CopyAndWaitAsync([NotNull] this ICommandsClient client, [NotNull] CopyFileRequest request, CancellationToken cancellationToken, int pullPeriod)
+        {
+            return CopyAndWaitAsync(client, request, cancellationToken, TimeSpan.FromSeconds(pullPeriod));
+        }
+
+
+        /// <summary>
+        /// Move file or folder on Disk from one path to another and wait until operation is done
+        /// </summary>
+        [Obsolete("Method is obsolete. Please use MoveAndWaitAsync(ICommandsClient, MoveFileRequest, CancellationToken, TimeSpan) instead.")]
+        public static Task MoveAndWaitAsync([NotNull] this ICommandsClient client, [NotNull] MoveFileRequest request, CancellationToken cancellationToken, int pullPeriod)
+        {
+            return MoveAndWaitAsync(client, request, cancellationToken, TimeSpan.FromSeconds(pullPeriod));
+        }
+
+        /// <summary>
+        /// Delete file or folder on Disk and wait until operation is done
+        /// </summary>
+        [Obsolete("Method is obsolete. Please use DeleteAndWaitAsync(ICommandsClient, DeleteFileRequest, CancellationToken, TimeSpan) instead.")]
+        public static Task DeleteAndWaitAsync([NotNull] this ICommandsClient client, [NotNull] DeleteFileRequest request, CancellationToken cancellationToken, int pullPeriod)
+        {
+            return DeleteAndWaitAsync(client, request, cancellationToken, TimeSpan.FromSeconds(pullPeriod));
+        }
+
+        /// <summary>
+        /// Empty trash
+        /// </summary>
+        [Obsolete("Method is obsolete. Please use EmptyTrashAndWaitAsyncAsync(ICommandsClient, string, CancellationToken, TimeSpan) instead.")]
+        public static Task EmptyTrashAndWaitAsyncAsync([NotNull] this ICommandsClient client, [NotNull] string path, CancellationToken cancellationToken, int pullPeriod)
+        {
+            return EmptyTrashAndWaitAsyncAsync(client, path, cancellationToken, TimeSpan.FromSeconds(pullPeriod));
+        }
+
+        /// <summary>
+        /// Restore files from trash
+        /// </summary>
+        [Obsolete("Method is obsolete. Please use RestoreFromTrashAndWaitAsyncAsync(ICommandsClient, RestoreFromTrashRequest, CancellationToken, TimeSpan) instead.")]
+        public static Task RestoreFromTrashAndWaitAsyncAsync([NotNull] this ICommandsClient client, [NotNull] RestoreFromTrashRequest request, CancellationToken cancellationToken, int pullPeriod)
+        {
+            return RestoreFromTrashAndWaitAsyncAsync(client, request, cancellationToken, TimeSpan.FromSeconds(pullPeriod));
+        }
+
+        #endregion
     }
 }
