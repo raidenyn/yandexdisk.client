643941897257a1169e3218c38809d3c67e0a1b31
Yuriy Nagaev <yuriy@nagaev.me>
2017-09-16 17:39:15 +0700

    Added separated nuspec file for CLI project.

---
+++ build-cli.bat
@@ -0,0 +1,20 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+
+set version=
+if not "%PackageVersion%" == "" (
+   set version=-Version %PackageVersion%
+)
+
+if "%nuget%" == "" (
+    set nuget=nuget
+)
+
+call .\build.bat %config%
+
+REM Package
+mkdir Build
+call %nuget% pack "src\YandexDisk.Client.CLI.nuspec" -symbols -o Build -p Configuration=%config% %version%
